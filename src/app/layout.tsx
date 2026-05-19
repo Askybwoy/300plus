abf4dacb7d66dec9f5a3742b0a2d738fe47f27f3
Aleksandr Dresviannikov <adresvyannikov@rtk-element.ru>
2026-05-19 19:19:25 +0700

    feat(analytics): add Yandex.Metrika tracking integration
    - Introduce YM_COUNTER_ID constant for Yandex.Metrika service
    - Add Script component to load Yandex.Metrika tag asynchronously
    - Initialize Yandex.Metrika with tracking options including webvisor and clickmap
    - Provide noscript fallback with tracking pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { SectionNav } from "@/components/ui/SectionNav";
 import { CookieBanner } from "@/components/ui/CookieBanner";
 
 const TMR_COUNTER_ID = "3766946";
+const YM_COUNTER_ID = 109303198;
 
 const inter = Inter({
   variable: "--font-inter",
@@ -67,6 +68,32 @@ export default function RootLayout({
               src={`https://top-fwz1.mail.ru/counter?id=${TMR_COUNTER_ID};js=na`}
               style={{ position: 'absolute', left: '-9999px' }}
               alt="Top.Mail.Ru"
+            />
+          </div>
+        </noscript>
+
+        {/* Yandex.Metrika counter */}
+        <Script
+          id="ym-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js?id=${YM_COUNTER_ID}", "ym");
+
+              ym(${YM_COUNTER_ID}, "init", {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+            `,
+          }}
+        />
+        <noscript>
+          <div>
+            <img
+              src={`https://mc.yandex.ru/watch/${YM_COUNTER_ID}`}
+              style={{ position: 'absolute', left: '-9999px' }}
+              alt=""
             />
           </div>
         </noscript>
